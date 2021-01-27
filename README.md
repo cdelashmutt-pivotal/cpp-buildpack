c2ee335df8686f52a704f745b6eaaaa2cba95174
Chris DeLashmutt <cdelashmutt@pivotal.io>
2021-01-27 18:15:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# C/C++ Buildpack
+A very simple Cloud Native Buildpack to call `make all`
---
+++ test/fixtures/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+  for(;;) {
+    printf("Hello, World!\n");
+    sleep(5);
+  }
+  return 0;
+}
